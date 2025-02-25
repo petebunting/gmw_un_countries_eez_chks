3aa2b3e6386df1926e0c89972e796f624b2eb4cf
Pete Bunting <petebunting@mac.com>
2025-02-25 19:43:49 +0000

    Updated.

--- 01_gmw_diff/01_calc_diff_north.py
+++ 01_gmw_diff/01_calc_diff_north.py
@@ -4,19 +4,21 @@ import glob
 import rsgislib.vectorutils
 import rsgislib.vectorgeoms
 
-gmw_vec_files = glob.glob("gmw_v4023_tiles/N*.gpkg")
+gmw_vec_files = glob.glob("/Users/pfb/Temp/gmw_v4_un_eez_chks/gmw_v4023_tiles/N*.gpkg")
 
-out_dir = "gmw_v4023_tiles_diff"
+out_dir = "/Users/pfb/Temp/gmw_v4_un_eez_chks/gmw_v4023_tiles_diff_20250225"
 
-un_cntry_vec_file = "eez_un_diss_20250213.gpkg"
-un_cntry_vec_lyr = "eez_un_diss_20250213"
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+
+un_cntry_vec_file = "mangrove_statistics_areas_20250225.gpkg"
+un_cntry_vec_lyr = "mangrove_statistics_areas_20250225"
 
 for gmw_vec_file in tqdm.tqdm(gmw_vec_files):
     gmw_vec_lyr = rsgislib.vectorutils.get_vec_lyrs_lst(gmw_vec_file)[0]
 
     out_vec_file = os.path.join(out_dir, f"{gmw_vec_lyr}_diff.gpkg")
     out_vec_lyr = f"{gmw_vec_lyr}_diff"
-    
     
     rsgislib.vectorgeoms.vec_lyr_difference_gp(
         vec_file=gmw_vec_file,
--- 01_gmw_diff/02_merge_gmw_intersects.py
+++ 01_gmw_diff/02_merge_gmw_intersects.py
@@ -2,8 +2,8 @@ import os
 import glob
 import rsgislib.vectorutils
 
-gmw_cntry_diffs = glob.glob("gmw_v4023_tiles_diff/*.gpkg")
+gmw_cntry_diffs = glob.glob("/Users/pfb/Temp/gmw_v4_un_eez_chks/gmw_v4023_tiles_diff_20250225/*.gpkg")
 
 
-rsgislib.vectorutils.merge_vector_files(vec_files = gmw_cntry_diffs, out_vec_file = "eez_un_diss_20250213_gmw_v4023_diff.geojson", out_vec_lyr = "eez_un_diss_20250213_gmw_v4023_diff", out_format = 'GeoJSON', out_epsg = None, remove_cols = None)
+rsgislib.vectorutils.merge_vector_files(vec_files = gmw_cntry_diffs, out_vec_file = "eez_un_diss_20250225_gmw_v4023_diff.geojson", out_vec_lyr = "eez_un_diss_20250225_gmw_v4023_diff", out_format = 'GeoJSON', out_epsg = None, remove_cols = None)
 
